d94b6c516cb38a6ddcf4f820917f3b52abb777d9
EkaterinaAlekseenko <e.alekseenko@bdgroup.biz>
2015-07-30 16:34:26 +0400

    add

--- protected/config/database.php
+++ protected/config/database.php
@@ -5,23 +5,23 @@ return array(
 //	'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
     // uncomment the following lines to use a MySQL database
 //
-//    'connectionString' => 'mysql:host=a0013854.xsph.ru;dbname=a0013854_kursoid',
-//    'emulatePrepare' => true,
-//    'username' => 'a0013854_kursoid',
-//    'password' => 'qwerty123',
-//    'charset' => 'utf8',
-//    // включаем профайлер
-//    'enableProfiling' => true,
-//    // показываем значения параметров
-//    'enableParamLogging' => true,
-
-    'connectionString' => 'mysql:host=localhost;dbname=kursoid',
+    'connectionString' => 'ssh okruble@ssh.okruble.nichost.ru',
     'emulatePrepare' => true,
-    'username' => 'root',
-    'password' => 'arahna1505',
+    'username' => 'okruble_mysql',
+    'password' => 'E:oUC2aF',
     'charset' => 'utf8',
     // включаем профайлер
     'enableProfiling' => true,
     // показываем значения параметров
     'enableParamLogging' => true,
+//
+//    'connectionString' => 'mysql:host=localhost;dbname=kursoid',
+//    'emulatePrepare' => true,
+//    'username' => 'okruble_db',
+//    'password' => 'arahna1505',
+//    'charset' => 'utf8',
+//    // включаем профайлер
+//    'enableProfiling' => true,
+//    // показываем значения параметров
+//    'enableParamLogging' => true,
 );
